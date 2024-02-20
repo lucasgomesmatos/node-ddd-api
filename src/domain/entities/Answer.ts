12891869afca8f32d469a98278cfb7c43589b80b
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-02-20 08:38:49 -0300

    feat: getters e setters

--- src/domain/entities/Answer.ts
+++ src/domain/entities/Answer.ts
@@ -11,10 +11,6 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
-  get content(): string {
-    return this.props.content;
-  }
-
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityId,
@@ -28,5 +24,37 @@ export class Answer extends Entity<AnswerProps> {
     );
 
     return answer;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.props.content.substring(0, 100).trimEnd().concat('...');
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
   }
 }
--- src/domain/entities/Question.ts
+++ src/domain/entities/Question.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { Entity } from '../../core/entities/Entity';
 import { UniqueEntityId } from '../../core/entities/UniqueEntityId';
 import { Optional } from '../../core/types/optinal';
@@ -15,17 +16,70 @@ interface QuestionProps {
 
 export class Question extends Entity<QuestionProps> {
   static create(
-    props: Optional<QuestionProps, 'createdAt'>,
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityId,
   ) {
     const question = new Question(
       {
         ...props,
+        slug: props.slug || Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id,
     );
 
     return question;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+  get slug() {
+    return this.props.slug;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew() {
+    return dayjs().diff(this.props.createdAt, 'days') <= 3;
+  }
+
+  get excerpt() {
+    return this.props.content.substring(0, 100).trimEnd().concat('...');
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityId | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
   }
 }
