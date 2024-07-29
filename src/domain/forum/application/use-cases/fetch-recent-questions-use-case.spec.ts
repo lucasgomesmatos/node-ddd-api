dc390d59de3ff1f8aca6ad6e05aab1bc4b10c0e1
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2024-07-28 23:47:57 -0300

    feat: fetch recent question pagination use case

--- src/domain/forum/application/use-cases/fetch-recent-questions-use-case.spec.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions-use-case.spec.ts
@@ -12,7 +12,7 @@ describe('Fetch Recent Questions', () => {
     sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository)
   })
 
-  test('should be able to fetch recent questions', async () => {
+  it('should be able to fetch recent questions', async () => {
     await inMemoryQuestionsRepository.create(
       makeQuestion({
         createdAt: new Date(2022, 0, 20),
@@ -38,5 +38,17 @@ describe('Fetch Recent Questions', () => {
       expect.objectContaining({ createdAt: new Date(2022, 0, 20) }),
       expect.objectContaining({ createdAt: new Date(2022, 0, 18) }),
     ])
+  })
+
+  it('should be able to fetch paginated recent questions', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(makeQuestion())
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    })
+
+    expect(questions).toHaveLength(2)
   })
 })
